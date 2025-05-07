ea62d893a2ad76637a76f63fabc04e4945f5b93c
Isabel <tizonarranz@gmail.com>
2025-05-07 17:06:57 +0200

    Navbar and its css created

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,44 @@
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
 export default function Navbar() {
 	return (
-		<>
-			<div>Hello Navbar</div>
-		</>
+		<nav className={styles.navbar}>
+			<div>
+				<Link to="/">Logo</Link>
+			</div>
+			<ul className={styles.navItems}>
+				<li className={styles.navItem}>
+					<Link className={styles.navLink} to="/">
+						Yoga
+					</Link>
+				</li>
+				<li className={styles.navItem}>
+					<Link className={styles.navLink} to="/">
+						Daily Menu
+					</Link>
+				</li>
+				<li className={styles.navItem}>
+					<Link className={styles.navLink} to="/">
+						Shop
+					</Link>
+				</li>
+				<li className={styles.navItem}>
+					<Link className={styles.navLink} to="/">
+						Contact
+					</Link>
+				</li>
+				<li className={styles.navItem}>
+					<Link className={styles.navLink} to="/">
+						Shopping Cart
+					</Link>
+				</li>
+				<li className={styles.navItem}>
+					<Link className={styles.navLink} to="/">
+						Sign In
+					</Link>
+				</li>
+			</ul>
+		</nav>
 	);
 }
