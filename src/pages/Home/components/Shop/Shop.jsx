8083ae10258fcfd30dbd5c95517db9a451980a18
Isabel <tizonarranz@gmail.com>
2025-05-09 15:01:22 +0200

    ImageTextBlock component added to Shop section in Home

--- src/pages/Home/components/Shop/Shop.jsx
+++ src/pages/Home/components/Shop/Shop.jsx
@@ -1,7 +1,15 @@
+import ImageTextBlock from "../../../../components/globals/ImageTextBlock/ImageTextBlock";
+import shopHome from "./assets/shopHome.jpg";
+
 export default function Shop() {
 	return (
 		<>
-			<h2>Hello Shop</h2>
+			<ImageTextBlock
+				title="Our Shop"
+				image={shopHome}
+				titleText="Joing our Retirements"
+				text="s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with "
+			/>
 		</>
 	);
 }
