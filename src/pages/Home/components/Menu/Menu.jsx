dad5a1b98b5aef08df7db70652dc74522554b8e4
Isabel <tizonarranz@gmail.com>
2025-05-10 09:35:40 +0200

    menu section updated with the TextBlock global component

--- src/pages/Home/components/Menu/Menu.jsx
+++ src/pages/Home/components/Menu/Menu.jsx
@@ -1,7 +1,19 @@
+import imgBlockMenu from "./assets/imgBlockMenu.jpg";
+import { Link } from "react-router-dom";
+import TextBlock from "../../../../components/globals/textBlock/textBlock";
+
 export default function Menu() {
 	return (
 		<>
-			<h2>Hello Menu</h2>
+			<TextBlock
+				title="Create your healthy daily Menu"
+				text="simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining esse"
+				backgroundImage={imgBlockMenu}
+			>
+				<Link to="/menu">
+					<button>Go to Menu</button>
+				</Link>
+			</TextBlock>
 		</>
 	);
 }
