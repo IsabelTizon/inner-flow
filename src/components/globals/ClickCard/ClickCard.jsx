363bf21d506612838d4e1bbbac7c3a549231b065
Isabel <tizonarranz@gmail.com>
2025-05-10 19:03:12 +0200

    ClickCard component created and added to Yoga section on home

---
+++ src/components/globals/ClickCard/ClickCard.jsx
@@ -0,0 +1,17 @@
+import styles from "./ClickCard.module.css";
+import { Link } from "react-router-dom";
+
+export default function ClickCard({ title, image, to }) {
+	return (
+		<Link to={to} className={styles.container}>
+			<img
+				className={styles.image}
+				src={image}
+				alt={title}
+			/>
+			<div className={styles.overlay}>
+				<h3 className={styles.titleCard}>{title}</h3>
+			</div>
+		</Link>
+	);
+}
--- src/pages/Home/components/Yoga/Yoga.jsx
+++ src/pages/Home/components/Yoga/Yoga.jsx
@@ -1,8 +1,12 @@
 import styles from "./Yoga.module.css";
+import ClickCard from "../../../../components/globals/ClickCard/ClickCard";
+import yogaPoses from "../../../../components/assets/yogaPoses.jpg";
+import createSequences from "../../../../components/assets/createSequences.png";
+import mySequences from "../../../../components/assets/mySequences.jpg";
 
 export default function Yoga() {
 	return (
-		<section>
+		<section className={styles.section}>
 			<div className={styles.titleContainer}>
 				<h2 className={styles.title}>Yoga</h2>
 			</div>
@@ -17,6 +21,23 @@ export default function Yoga() {
 					leap into electronic typesetting, remaining
 					essentially
 				</p>
+			</div>
+			<div className={styles.cardsContainer}>
+				<ClickCard
+					title="Poses"
+					image={yogaPoses}
+					to="yogaPoses"
+				/>
+				<ClickCard
+					title="Create Sequences"
+					image={createSequences}
+					to="createSequences"
+				/>
+				<ClickCard
+					title="My Sequences"
+					image={mySequences}
+					to="mySequences"
+				/>
 			</div>
 		</section>
 	);
