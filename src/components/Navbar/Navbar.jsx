c947e7fd2fc83438754a7247d92e0a2bd4603345
Isabel <tizonarranz@gmail.com>
2025-06-17 10:13:36 +0200

    user dropdown updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,20 @@
+// Components
 import { Link } from "react-router-dom";
+// Styles
 import styles from "./Navbar.module.css";
+// Assets
 import logo from "../../assets/logo.png";
+// hooks
 import useToggle from "../../hooks/useToggle";
 import useDropdown from "../../hooks/useDropdown";
+// Icons
+import { FaUser } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function Navbar() {
 	const [isOpen, toggle] = useToggle();
-	const yogaDropdown = useDropdown(); // aquí lo usas
+	const yogaDropdown = useDropdown();
+	const userDropdown = useDropdown();
 
 	return (
 		<nav className={styles.navbar}>
@@ -30,6 +38,7 @@ export default function Navbar() {
 					isOpen ? styles.showMenu : ""
 				}`}
 			>
+				{/* Yoga dropdown */}
 				<li className={styles.navItemWithDropdown}>
 					<span
 						className={styles.navLink}
@@ -60,30 +69,59 @@ export default function Navbar() {
 						</div>
 					)}
 				</li>
+				{/* Daily Menu */}
 				<li className={styles.navItem}>
 					<Link className={styles.navLink} to="/">
 						Daily Menu
 					</Link>
 				</li>
+				{/* Shop */}
 				<li className={styles.navItem}>
 					<Link className={styles.navLink} to="/">
 						Shop
 					</Link>
 				</li>
+				{/* Contact */}
 				<li className={styles.navItem}>
 					<Link className={styles.navLink} to="/">
 						Contact
 					</Link>
 				</li>
+				{/* Shopping Cart */}
 				<li className={styles.navItem}>
 					<Link className={styles.navLink} to="/">
-						Shopping Cart
+						<FaShoppingCart />
 					</Link>
 				</li>
-				<li className={styles.navItem}>
-					<Link className={styles.navLink} to="/">
-						Sign In
-					</Link>
+				{/* user register/login */}
+				<li className={styles.navItemWithDropdown}>
+					<span
+						className={styles.navLink}
+						onClick={userDropdown.toggle}
+						role="button"
+						aria-haspopup="true"
+						aria-expanded={userDropdown.isOpen}
+						title="Accede a tu cuenta"
+					>
+						<FaUser />
+					</span>
+
+					{userDropdown.isOpen && (
+						<div className={styles.dropdownMenu}>
+							<Link
+								to="/login"
+								onClick={userDropdown.close}
+							>
+								Login
+							</Link>
+							<Link
+								to="/register"
+								onClick={userDropdown.close}
+							>
+								Register
+							</Link>
+						</div>
+					)}
 				</li>
 			</ul>
 		</nav>
