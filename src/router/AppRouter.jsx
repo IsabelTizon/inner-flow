2083c76088c8b3ea81ba2f7ae28aa55adc3f2003
Isabel <tizonarranz@gmail.com>
2025-06-17 12:09:15 +0200

    renamed folders to follow lowercase naming convention

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,8 +3,8 @@ import {
 	Routes,
 	Route,
 } from "react-router-dom";
-import Home from "../pages/Home/Home";
-import Poses from "../pages/Yoga/Poses";
+import Home from "../pages/home/Home";
+import Poses from "../pages/yoga/Poses";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 
