33ac8902b94fe54dce328f80e6ea04a7e6d0be42
Isabel <tizonarranz@gmail.com>
2025-06-20 13:16:13 +0200

    register and login components created

---
+++ src/pages/users/Register.jsx
@@ -0,0 +1,38 @@
+import Btn from "../../components/globals/Buttons/Btn";
+
+export default function Register() {
+	return (
+		<div>
+			<div>
+				<Btn />
+			</div>
+			<form>
+				<label>Full Name</label>
+				<input
+					type="text"
+					placeholder="Enter your full name"
+				/>
+				<label>Password</label>
+				<input
+					type="password"
+					placeholder="Enter your password"
+				/>
+				<label>E-mail</label>
+				<input
+					type="email"
+					placeholder="Enter your e-mail address"
+				/>
+				<div className="terms">
+					<input type="checkbox" />
+					<span>
+						By signing up you agree{" "}
+						<a href="#">Terms & Conditions</a>
+					</span>
+				</div>
+				<div>
+					<Btn />
+				</div>
+			</form>
+		</div>
+	);
+}
