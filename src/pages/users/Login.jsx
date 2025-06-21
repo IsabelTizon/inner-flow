9af7a9b801c30cb3128b3e5a20e74eea62f0ceb5
Isabel <tizonarranz@gmail.com>
2025-06-21 11:21:49 +0200

    Login layout updated

--- src/pages/users/Login.jsx
+++ src/pages/users/Login.jsx
@@ -1,29 +1,54 @@
-// COMPONENTS
-import Btn from "../../components/globals/Buttons/Btn";
-
-// STYLES
+import LoginBox from "./components/LoginBox";
+import SVGBox from "./components/SVGBox";
 
 export default function Login() {
+	const LoginSVG = () => (
+		<svg
+			width="120"
+			height="120"
+			viewBox="0 0 120 120"
+			fill="none"
+		>
+			<path
+				d="M60 100 Q80 80 60 60 Q40 80 60 100"
+				stroke="#fff"
+				strokeWidth="3"
+				fill="none"
+			/>
+			<circle
+				cx="60"
+				cy="55"
+				r="8"
+				stroke="#fff"
+				strokeWidth="3"
+				fill="none"
+			/>
+			<path
+				d="M60 63 Q65 70 70 75"
+				stroke="#fff"
+				strokeWidth="3"
+				fill="none"
+			/>
+			<path
+				d="M60 63 Q55 70 50 75"
+				stroke="#fff"
+				strokeWidth="3"
+				fill="none"
+			/>
+		</svg>
+	);
 	return (
 		<div>
 			<div>
-				<Btn />
+				<SVGBox
+					img="../assets/logo.png"
+					title="Welcome back!!"
+					svg={LoginSVG}
+				/>
 			</div>
-			<form>
-				<label>Password</label>
-				<input
-					type="password"
-					placeholder="Enter your password"
-				/>
-				<label>E-mail</label>
-				<input
-					type="email"
-					placeholder="Enter your e-mail address"
-				/>
-				<div>
-					<Btn />
-				</div>
-			</form>
+			<div>
+				<LoginBox />
+			</div>
 		</div>
 	);
 }
