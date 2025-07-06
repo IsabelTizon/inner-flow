237b9a8eca4a88731941ff0b20f155aac6337a61
Isabel <tizonarranz@gmail.com>
2025-07-06 12:22:43 +0200

    registerBox updated

--- src/pages/users/auth/components/RegisterBox.jsx
+++ src/pages/users/auth/components/RegisterBox.jsx
@@ -1,38 +1,62 @@
 import Btn from "../../../../components/globals/Buttons/Btn";
+import { useRegister } from "../../../../hooks/useRegister";
+import styles from "../auth.module.css";
 
 export default function RegisterBox() {
+	const { handleRegister, error, loading } = useRegister();
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		const name = e.target[0].value;
+		const password = e.target[1].value;
+		const email = e.target[2].value;
+
+		handleRegister({ name, email, password });
+	};
 	return (
-		<div>
-			<div>
-				<Btn />
-			</div>
-			<form>
-				<label>Full Name</label>
+		<>
+			<div>{/* <Btn /> */}</div>
+			<form
+				onSubmit={onSubmit}
+				className={styles.formWrapper}
+			>
+				<label className={styles.label}>Full Name</label>
 				<input
 					type="text"
 					placeholder="Enter your full name"
+					className={styles.inputField}
 				/>
-				<label>Password</label>
+				<label className={styles.label}>Password</label>
 				<input
 					type="password"
 					placeholder="Enter your password"
+					className={styles.inputField}
 				/>
-				<label>E-mail</label>
+				<label className={styles.label}>E-mail</label>
 				<input
 					type="email"
 					placeholder="Enter your e-mail address"
+					className={styles.inputField}
 				/>
-				<div className="terms">
-					<input type="checkbox" />
+				<div className={styles.checkboxWrapper}>
+					<input
+						className={styles.checkbox}
+						type="checkbox"
+					/>
 					<span>
 						By signing up you agree{" "}
 						<a href="#">Terms & Conditions</a>
 					</span>
 				</div>
 				<div>
-					<Btn />
+					<Btn
+						text={loading ? "Registering..." : "Sign Up"}
+						type="submit"
+						variant="primary"
+					/>
 				</div>
+				{error && <p style={{ color: "red" }}>{error}</p>}
 			</form>
-		</div>
+		</>
 	);
 }
