b11a39b74abebed509a46e285003949188db6aae
Isabel <tizonarranz@gmail.com>
2025-07-06 13:03:30 +0200

    LoginBox updated

--- src/pages/users/auth/components/LoginBox.jsx
+++ src/pages/users/auth/components/LoginBox.jsx
@@ -1,29 +1,47 @@
 // COMPONENTS
 import Btn from "../../../../components/globals/Buttons/Btn";
+import { useLogIn } from "../../../../hooks/useLogIn";
 
 // STYLES
 import styles from "../auth.module.css";
 
 export default function Login() {
+	const { handleLogIn, error, loading } = useLogIn();
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		const password = e.target[1].value;
+		const email = e.target[2].value;
+
+		handleLogIn({ email, password });
+	};
 	return (
 		<div className={styles.containerAuth}>
-			<div>
-				<Btn />
-			</div>
-			<form>
-				<label>Password</label>
-				<input
-					type="password"
-					placeholder="Enter your password"
-				/>
-				<label>E-mail</label>
+			<div>{/* <Btn /> */}</div>
+			<form
+				onSubmit={onSubmit}
+				className={styles.formWrapper}
+			>
+				<label className={styles.label}>E-mail</label>
 				<input
 					type="email"
 					placeholder="Enter your e-mail address"
+					className={styles.inputField}
+				/>
+				<label className={styles.label}>Password</label>
+				<input
+					type="password"
+					placeholder="Enter your password"
+					className={styles.inputField}
 				/>
 				<div>
-					<Btn />
+					<Btn
+						text={loading ? "Log In..." : "Log In"}
+						type="submit"
+						variant="primary"
+					/>
 				</div>
+				{error && <p style={{ color: "red" }}>{error}</p>}
 			</form>
 		</div>
 	);
