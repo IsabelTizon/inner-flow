1b35951b54252f294c369a474793befcdbeaedf2
Isabel <tizonarranz@gmail.com>
2025-07-06 12:23:16 +0200

    useRegister Hook created

---
+++ src/hooks/useRegister.js
@@ -0,0 +1,48 @@
+// src/hooks/useRegister.js
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+export function useRegister() {
+	const navigate = useNavigate();
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(false);
+
+	const handleRegister = async ({
+		name,
+		email,
+		password,
+	}) => {
+		setLoading(true);
+		setError(null);
+
+		try {
+			const response = await fetch(
+				"http://localhost:3000/auth/register",
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ name, email, password }),
+				}
+			);
+
+			if (!response.ok) {
+				const errorData = await response.json();
+				throw new Error(
+					errorData.message || "Error en el registro"
+				);
+			}
+
+			// Éxito
+			alert("Usuario registrado con éxito");
+			navigate("/login");
+		} catch (err) {
+			setError(err.message);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	return { handleRegister, error, loading };
+}
