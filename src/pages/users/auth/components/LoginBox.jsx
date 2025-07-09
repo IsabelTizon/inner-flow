fe67856a1a89d99fcbf7043a3fda13a5c04c555a
Isabel <tizonarranz@gmail.com>
2025-07-09 16:05:49 +0200

    login and register updated using useState

--- src/pages/users/auth/components/LoginBox.jsx
+++ src/pages/users/auth/components/LoginBox.jsx
@@ -1,23 +1,22 @@
-// COMPONENTS
+import { useState } from "react";
 import Btn from "../../../../components/globals/Buttons/Btn";
 import { useLogIn } from "../../../../hooks/useLogIn";
-
-// STYLES
 import styles from "../auth.module.css";
 
 export default function Login() {
 	const { handleLogIn, error, loading } = useLogIn();
 
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
 	const onSubmit = (e) => {
 		e.preventDefault();
-		const password = e.target[1].value;
-		const email = e.target[2].value;
-
+		console.log("Login input:", email, password);
 		handleLogIn({ email, password });
 	};
+
 	return (
 		<div className={styles.containerAuth}>
-			<div>{/* <Btn /> */}</div>
 			<form
 				onSubmit={onSubmit}
 				className={styles.formWrapper}
@@ -27,12 +26,16 @@ export default function Login() {
 					type="email"
 					placeholder="Enter your e-mail address"
 					className={styles.inputField}
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
 				/>
 				<label className={styles.label}>Password</label>
 				<input
 					type="password"
 					placeholder="Enter your password"
 					className={styles.inputField}
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
 				/>
 				<div>
 					<Btn
--- src/pages/users/auth/components/RegisterBox.jsx
+++ src/pages/users/auth/components/RegisterBox.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Btn from "../../../../components/globals/Buttons/Btn";
 import { useRegister } from "../../../../hooks/useRegister";
 import styles from "../auth.module.css";
@@ -5,58 +6,62 @@ import styles from "../auth.module.css";
 export default function RegisterBox() {
 	const { handleRegister, error, loading } = useRegister();
 
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
 	const onSubmit = (e) => {
 		e.preventDefault();
-		const name = e.target[0].value;
-		const password = e.target[1].value;
-		const email = e.target[2].value;
-
 		handleRegister({ name, email, password });
 	};
+
 	return (
-		<>
-			<div>{/* <Btn /> */}</div>
-			<form
-				onSubmit={onSubmit}
-				className={styles.formWrapper}
-			>
-				<label className={styles.label}>Full Name</label>
+		<form
+			onSubmit={onSubmit}
+			className={styles.formWrapper}
+		>
+			<label className={styles.label}>Full Name</label>
+			<input
+				type="text"
+				placeholder="Enter your full name"
+				className={styles.inputField}
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+			/>
+			<label className={styles.label}>Password</label>
+			<input
+				type="password"
+				placeholder="Enter your password"
+				className={styles.inputField}
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+			/>
+			<label className={styles.label}>E-mail</label>
+			<input
+				type="email"
+				placeholder="Enter your e-mail address"
+				className={styles.inputField}
+				value={email}
+				onChange={(e) => setEmail(e.target.value)}
+			/>
+			<div className={styles.checkboxWrapper}>
 				<input
-					type="text"
-					placeholder="Enter your full name"
-					className={styles.inputField}
+					type="checkbox"
+					className={styles.checkbox}
 				/>
-				<label className={styles.label}>Password</label>
-				<input
-					type="password"
-					placeholder="Enter your password"
-					className={styles.inputField}
+				<span>
+					By signing up you agree{" "}
+					<a href="#">Terms & Conditions</a>
+				</span>
+			</div>
+			<div>
+				<Btn
+					text={loading ? "Registering..." : "Sign Up"}
+					type="submit"
+					variant="primary"
 				/>
-				<label className={styles.label}>E-mail</label>
-				<input
-					type="email"
-					placeholder="Enter your e-mail address"
-					className={styles.inputField}
-				/>
-				<div className={styles.checkboxWrapper}>
-					<input
-						className={styles.checkbox}
-						type="checkbox"
-					/>
-					<span>
-						By signing up you agree{" "}
-						<a href="#">Terms & Conditions</a>
-					</span>
-				</div>
-				<div>
-					<Btn
-						text={loading ? "Registering..." : "Sign Up"}
-						type="submit"
-						variant="primary"
-					/>
-				</div>
-				{error && <p style={{ color: "red" }}>{error}</p>}
-			</form>
-		</>
+			</div>
+			{error && <p style={{ color: "red" }}>{error}</p>}
+		</form>
 	);
 }
