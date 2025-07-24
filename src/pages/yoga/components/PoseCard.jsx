3dcbced767e53779509392fde701e382160c2d2b
Isabel <tizonarranz@gmail.com>
2025-07-24 13:47:48 +0200

    PoseCard created

---
+++ src/pages/yoga/components/PoseCard.jsx
@@ -0,0 +1,34 @@
+import useToggle from "../../../hooks/useToggle.js";
+import styles from "../Poses.module.css";
+import EyeBtn from "../../../components/globals/Buttons/EyeBtn.jsx";
+
+export default function PoseCard({
+	name,
+	image,
+	description,
+}) {
+	const [isOpen, toggle] = useToggle(false);
+	return (
+		<div className={styles.poseCard}>
+			<img
+				src={image}
+				alt={name}
+				className={styles.imgPoseCard}
+			/>
+			<h3 className={styles.titlePoseCard}>{name}</h3>
+			<EyeBtn onClick={toggle} />
+
+			{isOpen && (
+				<div
+					className={styles.popupOverlay}
+					onClick={toggle}
+				>
+					<div className={styles.popup}>
+						<p>{description}</p>
+						<button onClick={toggle}>Cerrar</button>
+					</div>
+				</div>
+			)}
+		</div>
+	);
+}
