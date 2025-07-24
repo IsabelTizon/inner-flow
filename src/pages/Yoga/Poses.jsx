26207ba2a5752789ac4760169bf13f05dc0c5b48
Isabel <tizonarranz@gmail.com>
2025-07-24 13:48:08 +0200

    poseCards mapped in Pose page

--- src/pages/Yoga/Poses.jsx
+++ src/pages/Yoga/Poses.jsx
@@ -1,3 +1,33 @@
+import PoseCard from "./components/PoseCard";
+import catPose from "./assets/cat-pose.jpg";
+
 export default function Poses() {
-	return <section></section>;
+	const poses = [
+		{
+			id: 1,
+			name: "Downward Dog",
+			image: catPose,
+			description: "Stretches legs and spine.",
+		},
+		{
+			id: 2,
+			name: "Tree Pose",
+			image: catPose,
+			description: "Improves balance and focus.",
+		},
+	];
+
+	return (
+		<div
+			style={{
+				display: "grid",
+				gap: "20px",
+				padding: "20px",
+			}}
+		>
+			{poses.map((pose) => (
+				<PoseCard key={pose.id} {...pose} />
+			))}
+		</div>
+	);
 }
