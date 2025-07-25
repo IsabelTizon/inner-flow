29d13331ab2b51e072c17215ad3bcff96ecf6821
Isabel <tizonarranz@gmail.com>
2025-07-25 16:32:47 +0200

    adminDashboard and its css created

---
+++ src/pages/admin/AdminDashboard.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import styles from "./AdminDashboard.module.css";
+
+function CardLink({ to, title, children }) {
+	return (
+		<Link to={to} className={styles.cardLink}>
+			<div className={styles.card}>
+				<h2 className={styles.cardTitle}>{title}</h2>
+				<div className={styles.cardContent}>{children}</div>
+			</div>
+		</Link>
+	);
+}
+
+export default function AdminDashboard() {
+	return (
+		<div className={styles.dashboard}>
+			<h1>Dashboard</h1>
+			<h2>Poses</h2>
+			<div className={styles.cards}>
+				<CardLink to="/admin/poses/create">
+					Create Pose
+				</CardLink>
+				<CardLink to="/admin/poses">
+					View All Poses
+				</CardLink>
+				{/* Más CardLink para cursos, shop, etc. */}
+			</div>
+		</div>
+	);
+}
