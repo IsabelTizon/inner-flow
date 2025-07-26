a793ffdf97a2e0a5dd050d57054f98acc80fe8af
Isabel <tizonarranz@gmail.com>
2025-07-26 09:49:23 +0200

    dailyMenu path updated on Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -73,7 +73,7 @@ export default function Navbar() {
 				</li>
 				{/* Daily Menu */}
 				<li className={styles.navItem}>
-					<Link className={styles.navLink} to="/">
+					<Link className={styles.navLink} to="/dailyMenu">
 						Daily Menu
 					</Link>
 				</li>
