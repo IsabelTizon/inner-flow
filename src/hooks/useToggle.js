c5f419a0148d1596a6700793be7273e788b138e4
Isabel <tizonarranz@gmail.com>
2025-07-30 17:47:40 +0200

    useToggle commented

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,7 +1,12 @@
+// src/hooks/useToggle.js
+// State
 import { useState } from "react";
 
+// initial: default value (false = closed)
 export default function useToggle(initial = false) {
-	const [isOpen, setIsOpen] = useState(initial);
+	const [isOpen, setIsOpen] = useState(initial); // Array Destructuring: [false, function]
+	// Function to toggle between true and false
+	// !curr: inverts the current value (true becomes false, false becomes true)
 	const toggle = () => setIsOpen((curr) => !curr);
 
 	return [isOpen, toggle];
