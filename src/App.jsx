9301e3771aa8bc3f46cbb036d1641814aa6e4349
Isabel <tizonarranz@gmail.com>
2025-07-31 10:37:50 +0200

    Global authentication context provider added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import AppRouter from "./router/AppRouter";
+import { AuthProvider } from "./context/AuthContext";
 
 export default function App() {
-	return <AppRouter />;
+	return (
+		// Global authentication context provider. Wrap your components into a context provider to specify the value of this context for all components inside
+		<AuthProvider>
+			<AppRouter />
+		</AuthProvider>
+	);
 }
