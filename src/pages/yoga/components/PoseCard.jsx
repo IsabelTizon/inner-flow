972321f69cb3960dd961b6106875530ef61ff797
Isabel <tizonarranz@gmail.com>
2025-08-08 16:23:25 +0200

    Refactored button to use Btn component with stopPropagation (e.stopPropagation())

--- src/pages/yoga/components/PoseCard.jsx
+++ src/pages/yoga/components/PoseCard.jsx
@@ -1,6 +1,7 @@
 import useToggle from "../../../hooks/useToggle.js";
 import styles from "./poseCard.module.css";
 import EyeBtn from "../../../components/globals/Buttons/EyeBtn.jsx";
+import Btn from "../../../components/globals/Buttons/Btn.jsx";
 
 export default function PoseCard({
 	name,
@@ -18,14 +19,30 @@ export default function PoseCard({
 			<h3 className={styles.titlePoseCard}>{name}</h3>
 			<EyeBtn onClick={toggle} />
 
+			{/* POP UP */}
 			{isOpen && (
 				<div
 					className={styles.popupOverlay}
 					onClick={toggle}
 				>
 					<div className={styles.popup}>
+						<img
+							src={`http://localhost:3000/img/poses/${image}`}
+							alt={name}
+							className={styles.imgPoseCardPopup}
+						/>
+						<h3 className={styles.popupTitle}>{name}</h3>
 						<p>{description}</p>
-						<button onClick={toggle}>Cerrar</button>
+						<Btn
+							text="Close"
+							variant="secondary"
+							onClick={(e) => {
+								e.stopPropagation();
+								toggle();
+							}}
+						>
+							Close
+						</Btn>
 					</div>
 				</div>
 			)}
