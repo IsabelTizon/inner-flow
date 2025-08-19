1f46ee4fbb4a19410200bb0bd12eb6a1e41b7d00
Isabel <tizonarranz@gmail.com>
2025-08-19 17:02:55 +0200

    Add LogoBox component to Footer
    Introduces the LogoBox component for the Footer, displaying the site logo, title, and a brief description. This enhances branding and provides a quick link to the homepage.

---
+++ src/components/Footer/components/LogoBox.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+// Styles
+import styles from "../footer.module.css";
+// Assets
+import logo from "../../../assets/logo.png";
+
+export default function LogoBox() {
+	return (
+		<>
+			<div className={styles.section}>
+				<Link to="/">
+					<img
+						className={styles.logo}
+						src={logo}
+						alt="logo icon"
+						width={80}
+					/>
+				</Link>
+				<h2 className={styles.logo}>Inner-flow</h2>
+				<p className={styles.description}>
+					Your space to practice yoga, explore healthy
+					living, and connect with the community.
+				</p>
+			</div>
+		</>
+	);
+}
