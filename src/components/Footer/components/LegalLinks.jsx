da7988828c27061c976c0bd1c00a8459bcf4ff84
Isabel <tizonarranz@gmail.com>
2025-08-19 17:03:01 +0200

    Add LegalLinks component to footer
    Introduces a new LegalLinks component displaying links to Terms & Conditions, Privacy Policy, and Cookies Policy in the footer, styled with footer.module.css and using react-router-dom Link.

---
+++ src/components/Footer/components/LegalLinks.jsx
@@ -0,0 +1,29 @@
+// Router
+import { Link } from "react-router-dom";
+// Styles
+import styles from "../footer.module.css";
+
+export default function LegalLinks() {
+	return (
+		<div className={styles.legalLinks}>
+			<h3 className={styles.title}>LEGAL</h3>
+			<ul className={styles.list}>
+				<li>
+					<Link className={styles.navLink} to="/terms">
+						Terms & Conditions
+					</Link>
+				</li>
+				<li>
+					<Link className={styles.navLink} to="/privacy">
+						Privacy Policy
+					</Link>
+				</li>
+				<li>
+					<Link className={styles.navLink} to="/cookies">
+						Cookies Policy
+					</Link>
+				</li>
+			</ul>
+		</div>
+	);
+}
