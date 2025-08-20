40c2a0a0c844fad68b797be32c80445edca6cdb1
Isabel <tizonarranz@gmail.com>
2025-08-20 16:02:29 +0200

    Comment out Shop component in Home page
    The Shop component import and usage have been commented out in Home.jsx. This temporarily removes the Shop section from the Home page, possibly for maintenance or redesign.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Yoga from "./components/Yoga/Yoga";
 import Menu from "./components/Menu/Menu";
-import Shop from "./components/Shop/Shop";
+// import Shop from "./components/Shop/Shop";
 import Contact from "./components/Contact/Contact";
 
 export default function Home() {
@@ -10,9 +10,9 @@ export default function Home() {
 		<>
 			<Hero />
 			<About />
-			<Yoga />
 			<Menu />
-			<Shop />
+			<Yoga />
+			{/* <Shop /> */}
 			<Contact />
 		</>
 	);
