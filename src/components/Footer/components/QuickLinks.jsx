17be7bf01ef51649e12cdeb897233bc419092dea
Isabel <tizonarranz@gmail.com>
2025-08-25 16:13:34 +0200

    Fix invalid HTML nesting by removing <li> inside <li> to prevent hydration error

--- src/components/Footer/components/QuickLinks.jsx
+++ src/components/Footer/components/QuickLinks.jsx
@@ -8,52 +8,46 @@ export default function QuickLinks() {
 			<div className={styles.section}>
 				<h3 className={styles.title}>QUICK LINKS</h3>
 				<ul className={styles.list}>
-					<li>
-						<li className={styles.navItem}>
-							<Link className={styles.navLink} to="/">
-								Home
-							</Link>
-						</li>
+					<li className={styles.navItem}>
+						<Link className={styles.navLink} to="/">
+							Home
+						</Link>
 					</li>
-					<li>
-						<li className={styles.navItem}>
-							<Link
-								className={styles.navLink}
-								to="/dailyMenu"
-							>
-								Daily Menu
-							</Link>
-						</li>
+
+					<li className={styles.navItem}>
+						<Link
+							className={styles.navLink}
+							to="/dailyMenu"
+						>
+							Daily Menu
+						</Link>
 					</li>
-					<li>
-						<li className={styles.navItem}>
-							<Link
-								className={styles.navLink}
-								to="/yoga/poses"
-							>
-								Yoga Poses
-							</Link>
-						</li>
+
+					<li className={styles.navItem}>
+						<Link
+							className={styles.navLink}
+							to="/yoga/poses"
+						>
+							Yoga Poses
+						</Link>
 					</li>
-					<li>
-						<li className={styles.navItem}>
-							<Link
-								className={styles.navLink}
-								to="/yoga/sequences"
-							>
-								Sequences
-							</Link>
-						</li>
+
+					<li className={styles.navItem}>
+						<Link
+							className={styles.navLink}
+							to="/yoga/sequences"
+						>
+							Sequences
+						</Link>
 					</li>
-					<li>
-						<li className={styles.navItem}>
-							<Link
-								className={styles.navLink}
-								to="/community"
-							>
-								Community Sequences
-							</Link>
-						</li>
+
+					<li className={styles.navItem}>
+						<Link
+							className={styles.navLink}
+							to="/community"
+						>
+							Community Sequences
+						</Link>
 					</li>
 
 					<li className={styles.navItem}>
