c9a1d7a94caec4000b88d42417ac92b959e9a640
Isabel <tizonarranz@gmail.com>
2025-08-25 16:02:47 +0200

    correct register endpoint in fetch request

--- src/hooks/useRegister.js
+++ src/hooks/useRegister.js
@@ -16,16 +16,13 @@ export function useRegister() {
 		setError(null);
 
 		try {
-			const response = await fetch(
-				"http:///users/auth/register",
-				{
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({ name, email, password }),
-				}
-			);
+			const response = await fetch("http://auth/register", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ name, email, password }),
+			});
 
 			if (!response.ok) {
 				const errorData = await response.json();
